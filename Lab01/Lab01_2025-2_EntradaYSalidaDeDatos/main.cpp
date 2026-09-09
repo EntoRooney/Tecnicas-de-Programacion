46cad22bd0c117759c3a8fac29b951d619821c53
Ento Lazaro <entolazaro@gmail.com>
2026-09-09 00:45:23 -0500

    Agregar Lab01_2025-2

---
+++ Lab01/Lab01_2025-2_EntradaYSalidaDeDatos/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+//Ento Rooney Lazaro Trujillo
+//202202032
+
+using namespace std ;
+#include "Bibliotecas/funciones.h"
+//este programa tiene el fin de generar una boleta de un resumen de reproducciones de canales de twicht que
+//cumplan una fecha limite y ver cuanto generó la cantidad de reproducciones ademas al final nos piden
+//un resumen final para calcular cuanto fue el ingreso y quien fue el mayor
+int main() {
+    leerDatos();
+    return 0;
+}
